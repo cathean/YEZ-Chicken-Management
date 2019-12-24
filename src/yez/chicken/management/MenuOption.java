e696a1825d306099a0580ac75dbd168bfd419dd8
Cathean <ivanshadow19@gmail.com>
2019-12-24 16:58:58 +0700

    Signed-off-by: Cathean <ivanshadow19@gmail.com>

---
+++ src/yez/chicken/management/MenuOption.java
@@ -0,0 +1,48 @@
+package yez.chicken.management;
+
+public class MenuOption
+{
+    public void mainMenu()
+    {
+        
+    }
+    
+    public void orderMenu()
+    {
+        
+    }
+    
+    public void showOrderMenu()
+    {
+        
+    }
+    
+    public void addOrderMenu()
+    {
+        
+    }
+    
+    public void delOrderMenu()
+    {
+    }
+    
+    public void productMenu()
+    {
+        
+    }
+    
+    public void showProductMenu()
+    {
+        
+    }
+    
+    public void addProductMenu()
+    {
+        
+    }
+    
+    public void delProductMenu()
+    {
+        
+    }
+}
---
+++ src/yez/chicken/management/YEZChickenManagement.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yez.chicken.management;
+
+/**
+ *
+ * @author ivans
+ */
+public class YEZChickenManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
